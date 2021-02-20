1cf96f3fde3aa5c6548f5aff36ac60c9a3734a15
ryotanaka <sekaiitiousama@gmail.com>
2021-02-20 21:04:53 +0900

    mp3を再生するように変更

--- app/src/main/java/com/example/avocado/MainActivity.kt
+++ app/src/main/java/com/example/avocado/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.avocado
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private var mediaPlayer: MediaPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.avocado)
+        val button = findViewById<Button>(R.id.avocadoButton)
+        button.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        mediaPlayer?.start()
     }
 }
